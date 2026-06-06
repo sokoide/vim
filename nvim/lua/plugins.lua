74cc531b9bb6d9d672229578b8c7d0f2165b6b49
So Koide <sokoide@gmail.cojm>
2026-06-06 15:22:35 +0900

    add: MdTableAlignAll

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -552,6 +552,26 @@ return {
 		end,
 		config = function()
 			vim.cmd("TableModeEnable")
+
+			-- カレントバッファの全テーブルを一括整列
+			vim.api.nvim_create_user_command("MdTableAlignAll", function()
+				local winview = vim.fn.winsaveview()
+				local total = vim.fn.line("$")
+				local cur = 1
+				while cur <= total do
+					if vim.fn.getline(cur):match("^%s*|") then
+						vim.fn.cursor(cur, 1)
+						vim.cmd("TableModeRealign")
+						total = vim.fn.line("$")
+						while cur <= total and vim.fn.getline(cur):match("^%s*|") do
+							cur = cur + 1
+						end
+					else
+						cur = cur + 1
+					end
+				end
+				vim.fn.winrestview(winview)
+			end, { desc = "Realign all markdown tables in buffer" })
 		end,
 	},
 	-- markdown preview
